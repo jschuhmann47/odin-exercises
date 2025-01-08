85953fd2ae3122e3dad55056cceb5fa28823c761
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2025-01-08 20:21:28 -0300

    add new grid button

--- sketchpad/sketchpad.js
+++ sketchpad/sketchpad.js
@@ -8,8 +8,8 @@ for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < ROWS; j++) {
         const newSquare = document.createElement("div")
         newSquare.classList.add("square")
-        newSquare.addEventListener('mouseover', function(event) {
-            newSquare.style.backgroundColor = "red"
+        newSquare.addEventListener('mouseover', function (event) {
+            newSquare.style.backgroundColor = "purple"
         })
         // newSquare.addEventListener('mouseleave', function(event){
         //     setTimeout(() => {
@@ -19,4 +19,11 @@ for (let i = 0; i < ROWS; i++) {
         newRow.appendChild(newSquare)
     }
     mainGrid.appendChild(newRow)
+}
+
+function newGrid() {
+    const userSize = parseInt(prompt("How many rows? Min 2, max 100:"))
+    const rows = Math.max(Math.min(userSize, 100), 2)
+    console.log(rows)
+    // TODO: regenerate rows
 }
