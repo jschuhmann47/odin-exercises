0f51afc34b2a00a0ee7baa6c982952c7ec3e2e54
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2025-01-08 20:02:37 -0300

    add mouseover event

--- sketchpad/sketchpad.js
+++ sketchpad/sketchpad.js
@@ -8,6 +8,14 @@ for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < ROWS; j++) {
         const newSquare = document.createElement("div")
         newSquare.classList.add("square")
+        newSquare.addEventListener('mouseover', function(event) {
+            newSquare.style.backgroundColor = "red"
+        })
+        // newSquare.addEventListener('mouseleave', function(event){
+        //     setTimeout(() => {
+        //         newSquare.style.backgroundColor = "pink"
+        //     }, 1000)
+        // })
         newRow.appendChild(newSquare)
     }
     mainGrid.appendChild(newRow)
