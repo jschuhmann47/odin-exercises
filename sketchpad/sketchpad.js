14d9f4271019aa80f39b568963b291e6e48ddb2c
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2025-01-10 07:47:13 -0300

    generate new boards

--- sketchpad/sketchpad.js
+++ sketchpad/sketchpad.js
@@ -2,28 +2,27 @@ const ROWS = 4
 
 const mainGrid = document.getElementById("main-grid")
 
-for (let i = 0; i < ROWS; i++) {
-    const newRow = document.createElement("div")
-    newRow.classList.add("row")
-    for (let j = 0; j < ROWS; j++) {
-        const newSquare = document.createElement("div")
-        newSquare.classList.add("square")
-        newSquare.addEventListener('mouseover', function (event) {
-            newSquare.style.backgroundColor = "purple"
-        })
-        // newSquare.addEventListener('mouseleave', function(event){
-        //     setTimeout(() => {
-        //         newSquare.style.backgroundColor = "pink"
-        //     }, 1000)
-        // })
-        newRow.appendChild(newSquare)
-    }
-    mainGrid.appendChild(newRow)
-}
+generateSquares(ROWS)
 
 function newGrid() {
     const userSize = parseInt(prompt("How many rows? Min 2, max 100:"))
     const rows = Math.max(Math.min(userSize, 100), 2)
-    console.log(rows)
-    // TODO: regenerate rows
+    mainGrid.textContent = '';
+    generateSquares(rows)
+}
+
+function generateSquares(rows){
+    for (let i = 0; i < rows; i++) {
+        const newRow = document.createElement("div")
+        newRow.classList.add("row")
+        for (let j = 0; j < rows; j++) {
+            const newSquare = document.createElement("div")
+            newSquare.classList.add("square")
+            newSquare.addEventListener('mouseover', function (event) {
+                newSquare.style.backgroundColor = "purple"
+            })
+            newRow.appendChild(newSquare)
+        }
+        mainGrid.appendChild(newRow)
+    }
 }
